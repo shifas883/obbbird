344353902c472e19623b29286efb255877a73dd7
shifas <shifasmeharnk@gmail.com>
2021-11-17 13:43:45 +0530

    bird

---
+++ bin/obbbird.dart
@@ -0,0 +1,35 @@
+
+void main() {
+
+  final bd=Bird("parr", 55);
+  bd.name="parrot";
+  bd.weight=4;
+
+  bd.flying();
+  bd.walking();
+  bd.flying();
+  bd.flying();
+}
+
+class Bird{
+  var name;
+  var weight;
+  var fly;
+
+
+  Bird(String name,var weight){
+    this.name=name;
+    weight=weight;
+
+    fly=0;
+
+  }
+ void flying(){
+    fly++;
+    print("The $name is fly=$fly meter");
+ }
+ void walking(){
+    print("$name is walking");
+
+ }
+}
